8a52f0aba9bcb4c888c587a0121e56e476f788a4
max <maximumbalk@gmail.com>
2019-04-29 16:51:02 -0500

    forgot foreign key

--- init-forest.sql
+++ init-forest.sql
@@ -48,3 +48,9 @@ create table Contains_species (
     primary key (Species_name, cell_id)
 );
 
+alter table Forest_location add constraint fk_Forest_name foreign key (Forest_name) references Forest (Official_name);
+alter table Cell add constraint fk_cell_forest foreign key (Forest_name) references Forest (Official_name);
+alter table Cell add constraint fk_cell_climate foreign key (Climate_name) references Climate (Climate_name);
+alter table Contains_species add constraint fk_species_relation foreign key (Species_name) references Tree_species (Scientific_name);
+alter table Contains_species add constraint fk_cell_relation foreign key (cell_id) references Cell (id);
+
