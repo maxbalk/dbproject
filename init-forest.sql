78a60b6280e63d63293d7ce80fddf736f2059159
max <maximumbalk@gmail.com>
2019-05-04 21:49:01 -0500

    new update rules for fk

--- init-forest.sql
+++ init-forest.sql
@@ -49,9 +49,9 @@ create table Contains_species (
 
 alter table Forest_location add constraint foreign key (Forest_name) references Forest (Official_name) ON UPDATE CASCADE ON DELETE CASCADE;
 alter table Cell add constraint fk_cell_forest foreign key (Forest_name) references Forest (Official_name) ON DELETE CASCADE ON UPDATE CASCADE;
-alter table Cell add constraint fk_cell_climate foreign key (Climate_name) references Climate (Climate_name);
+alter table Cell add constraint fk_cell_climate foreign key (Climate_name) references Climate (Climate_name) ON DELETE SET NULL;
 alter table Contains_species add constraint fk_species_relation foreign key (Species_name) references Tree_species (Scientific_name);
-alter table Contains_species add constraint fk_cell_relation foreign key (cell_id) references Cell (id);
+alter table Contains_species add constraint fk_cell_relation foreign key (cell_id) references Cell (id) ON DELETE CASCADE;
 
 create user 'forestManager'@'%'
     identified by 'AUPW1234';
